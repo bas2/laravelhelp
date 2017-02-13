3fc9930301d6951440e2c83c23730ab1f3827ffc
bas2 <me@bashir.biz>
2017-02-13 17:18:27 +0000

    Created getTopicSubtopics.

--- app/Subtopic.php
+++ app/Subtopic.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subtopic extends Model
 {
-    //
+    /**
+     * Return all subtopics from topic.
+     *
+     * @param  integer|null  $topicid
+     * @return ?
+     */
+    public static function getTopicSubtopics($topicid) {
+      return static::where('topicid',$topicid)->latest('updated_at')->get(['stopic_id','stopic']);
+    }
 }
