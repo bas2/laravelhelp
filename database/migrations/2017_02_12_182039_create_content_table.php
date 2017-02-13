e9f059e1cab6da35b71a471d5e8ddc82788c4bc9
bas2 <me@bashir.biz>
2017-02-13 17:46:53 +0000

    New commit.

---
+++ database/migrations/2017_02_12_182039_create_content_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content', function (Blueprint $table) {
+            $table->increments('content_id')->unsigned();
+            $table->integer('stopicid')->unsigned();
+            $table->integer('parentid')->unsigned();
+            $table->integer('groupid')->unsigned();
+            $table->integer('controllerid')->unsigned();
+            $table->string('title');
+            $table->text('content');
+            $table->boolean('pintonav');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content');
+    }
+}
