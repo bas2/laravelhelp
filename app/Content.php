094c017089f4f02c2151c3efa018b8e11fe648a2
bas2 <me@bashir.biz>
2017-02-14 16:39:16 +0000

    Modified getContent to accept an orderby to allow main articles to display in two different orders.

--- app/Content.php
+++ app/Content.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Content extends Model
 {
-    public static function getContent($subtopicid) {
+    public static function getContent($subtopicid, $orderby='moddate') {
+      $orderby = ($orderby=='oname') ? 'concat_ws(" ",groups.name,contents.title)' 
+      : 'updated_at desc' ;
       return static::where('contents.stopicid',$subtopicid)
       ->leftJoin('groups','contents.groupid','=','groups.group_id')
       ->where('parentid', 0)
-      ->latest('contents.updated_at')
+      ->orderByRaw($orderby)
       ->get(['groups.name', 'contents.title', 'content_id']);
     }
 
