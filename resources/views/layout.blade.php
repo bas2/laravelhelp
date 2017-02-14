0dc7d4cb62fb5a843def6dbac01bfa5be5ecb9f5
bas2 <me@bashir.biz>
2017-02-14 21:05:05 +0000

    New commit.

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+  <meta charset="utf-8">
+  <title>PHP App::Help!</title>
+  <link rel="stylesheet" href="/help/public/css/styles.css">
+
+@yield('mainheader')
+
+</head>
+<body>
+
+@yield('content')
+
+  </body>
+</html>
