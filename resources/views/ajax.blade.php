7c96c7e0aac8fec110925c81853ab10157d4e3f7
bas2 <me@bashir.biz>
2017-02-20 18:24:26 +0000

    On the article div, added edit and close buttons. Formatted created and updated datetimes. When editing an article, anchor the main article. Change due to adding .expand class to expand link. Jquery for editing and closing article div.

--- resources/views/ajax.blade.php
+++ resources/views/ajax.blade.php
@@ -1,6 +1,6 @@
 <div class="divartcontent">
 
-  <div><em>{{ $content->content_id }} / {{ $content->created_at }} / {{ $content->updated_at }}</em><span title="Expand">&uarr;</span></div>
+  <div><em>{{ $content->content_id }} / {{ \Carbon\Carbon::parse($content->created_at)->format('d/m/Y H:i') }} / {{ \Carbon\Carbon::parse($content->updated_at)->format('d/m/Y H:i') }}</em><span class="edit">E</span> <span class="close">X</span> <span class="expand" title="Expand">&uarr;</span></div>
   <div>{!! $content->content !!}</div>
 
   <div class="article_btns">
@@ -15,22 +15,26 @@ $(document).ready(function(){
   // Edit article - show form when clicking Edit.
   $('#editarticle').die('click').live('click', function() {
     var articleid = $(this).attr('title');
+    // Anchor the article title:
+    var article = $(this).parent().parent().parent().parent().find('a');
+    if (article.attr('class')=='subbarticle') { // Reply.
+      article = article.parent().parent().parent().find('a');
+    }
     $.ajax({
       "type":"GET",
       "url":"ajax/harticleedit/" + parseInt(articleid),
       //"data": 'id=' + parseInt(articleid),
       "success":function(data){
         $('#articlediv').html(data).fadeIn('slow');
-
         //if ($('#helpcontent').val().length==0) {$('#helpcontent').focus();}
         //if ($('#helptitle').val().length==0)  {$('#helptitle').focus();}      
       } // End ajax success function
     });
-    $('html, body').animate({scrollTop: $('#articlediv').offset().top}, 0);
+    $('html, body').animate({scrollTop: article.offset().top}, 0);
   });
 
   // Click option to expand article.
-  $('.divartcontent span').click(function(){
+  $('.divartcontent span.expand').click(function(){
     var parentdiv = $(this).parent().parent().parent(); // #divarticle
     parentdiv.toggleClass('togglewidth');
     if (parentdiv.hasClass('togglewidth')) {
@@ -47,6 +51,21 @@ $(document).ready(function(){
       $('body, html').css('overflow','auto');
     }
   });
+
+  $('.divartcontent span.close').click(function(){
+    $('#articlediv').remove();
+  });
+
+  $('.divartcontent span.edit').click(function(){
+    var articleid = $(this).parent().parent().parent().parent().find('a').attr('title2');
+    $.ajax({
+      "type":"GET",
+      "url":"ajax/harticleedit/" + parseInt(articleid),
+      "success":function(data){$('#articlediv').html(data).fadeIn('slow');}
+    });
+  });
+
+
 
 
 });
