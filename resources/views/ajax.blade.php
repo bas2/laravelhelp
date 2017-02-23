865943e0287ff19370695e73daf86a6060964a51
bas2 <me@bashir.biz>
2017-02-23 18:39:00 +0000

    Have put article option icons in a ul with class articleoptions instead of in spans. Removal of commented out code. Jquery change from span to li. Hide close option in overlay since contract does the same thing. Show it again when overlay closes. Comments to explain jquery functions.

--- resources/views/ajax.blade.php
+++ resources/views/ajax.blade.php
@@ -1,11 +1,18 @@
 <div class="divartcontent">
+  
+  <ul class="articleoptions">
+    <li class="edit"><img src="img/write.png" width="15"></li> 
+    <li class="close"><img src="img/close.png" width="16"></li> 
+    <li class="expand" title="Expand"><img src="img/expand.png" width="14"></li>
+  </ul>
 
-  <div><em>{{ $content->content_id }} / {{ \Carbon\Carbon::parse($content->created_at)->format('d/m/Y H:i') }} / {{ \Carbon\Carbon::parse($content->updated_at)->format('d/m/Y H:i') }}</em><span class="edit"><img src="img/write.png" width="15"></span> <span class="close"><img src="img/close.png" width="16"></span> <span class="expand" title="Expand"><img src="img/expand.png" width="14"></span></div>
+  <div><em>{{ $content->content_id }} {{ \Carbon\Carbon::parse($content->created_at)->format('d/m/Y H:i') }} {{ \Carbon\Carbon::parse($content->updated_at)->format('d/m/Y H:i') }}</em></div>
+
   <div>{!! $content->content !!}</div>
 
   <div class="article_btns">
-  <input id="editarticle" class="active" title="{{ $content->content_id }}" type="button" value="Edit &gt;">
-  <input id="closeeditarticle"                          type="button" value="Close &gt;">
+    <input id="editarticle" class="active" title="{{ $content->content_id }}" type="button" value="Edit &gt;">
+    <input id="closeeditarticle" type="button" value="Close &gt;">
   </div>
 
 </div>
@@ -23,7 +30,6 @@ $(document).ready(function(){
     $.ajax({
       "type":"GET",
       "url":"ajax/harticleedit/" + parseInt(articleid),
-      //"data": 'id=' + parseInt(articleid),
       "success":function(data){
         $('#articlediv').html(data).fadeIn('slow');
         //if ($('#helpcontent').val().length==0) {$('#helpcontent').focus();}
@@ -34,7 +40,7 @@ $(document).ready(function(){
   });
 
   // Click option to expand article.
-  $('.divartcontent span.expand').click(function(){
+  $('.divartcontent li.expand').click(function(){
     var parentdiv = $(this).parent().parent().parent(); // #divarticle
     parentdiv.toggleClass('togglewidth');
     if (parentdiv.hasClass('togglewidth')) {
@@ -43,21 +49,25 @@ $(document).ready(function(){
       $('<div class="prepended">'+subtopic+' &rarr; '+article_name+'</div>').prependTo($(this).parent().parent());
       $(this).attr('title','Contract').html('<img src="img/expand2.png">');
       $('.article_btns').hide();
+      $('li img[src*=close]').hide();
       $('body, html').css('overflow','hidden');
     } else {
       $(this).attr('title','Expand').html('<img src="img/expand.png" width="16">');
       $('.article_btns').show();
+      $('li img[src*=close]').show();
       $('div.prepended').remove();
       $('body, html').css('overflow','auto');
     }
   });
 
-  $('.divartcontent span.close').click(function(){
+  // Close article in overlay.
+  $('.divartcontent li.close').click(function(){
     $('#articlediv').remove();
     $('body, html').css('overflow','auto');
   });
 
-  $('.divartcontent span.edit').click(function(){
+  // Edit article in overlay.
+  $('.divartcontent li.edit').click(function(){
     var articleid = $(this).parent().parent().parent().parent().find('a').attr('title2');
     $.ajax({
       "type":"GET",
@@ -65,8 +75,6 @@ $(document).ready(function(){
       "success":function(data){$('#articlediv').html(data).fadeIn('slow');}
     });
   });
-
-
 
 
 });
