48f32fa6967400cc034a55310a5482a60eca3392
bas2 <me@bashir.biz>
2017-03-16 19:53:52 +0000

    Changed route paths to be more RESTful?

--- routes/web.php
+++ routes/web.php
@@ -5,32 +5,32 @@ use App\Http\Requests;
 Route::get('home', 'HelpController@index');
 
 // Complete article update:
-Route::post('ajax/harticleedit/{id}', 'HelpController@postArticleEdit');
+Route::post('article/edit/{id}', 'HelpController@postArticleEdit');
 
 // Display article edit form.
-Route::get('ajax/harticleedit/{id}', 'HelpController@getArticleEdit');
+Route::get('article/edit/{id}', 'HelpController@getArticleEdit');
 
 // Get subtopic articles:
-Route::get('ajax/content/{id}', 'HelpController@articles' );
+Route::get('article/subtopic/{id}', 'HelpController@articles' );
 
 // Get article contents.
-Route::get('ajax/{id}', 'HelpController@article');
+Route::get('article/{id}', 'HelpController@article');
 
 // Add new reply:
-Route::get('ajax/hreplytoarticle/{id}', 'HelpController@replyToArticle');
+Route::get('article/reply/{id}', 'HelpController@replyToArticle');
 
 // Delete article
-Route::get('ajax/hdelarticle/{id}', 'HelpController@deleteArticle');
+Route::get('article/delete/{id}', 'HelpController@deleteArticle');
 
 // New article
-Route::get('ajax/hnewarticle/{id}', 'HelpController@addArticle' );
+Route::get('article/new/subtopic/{id}', 'HelpController@addArticle' );
 
 
 // New subtopic.
-Route::get('stopic/new/{id}', 'HelpController@getAddSubtopic');
+Route::get('subtopic/new/{id}', 'HelpController@getAddSubtopic');
 
 // New subtopic:
-Route::post('stopic/new/{id}', 'HelpController@addSubtopic');
+Route::post('subtopic/new/{id}', 'HelpController@addSubtopic');
 
 // Rename or delete subtopic page:
 Route::get('subtopic/{id}', 'HelpController@getSubtopicActions');
@@ -40,7 +40,7 @@ Route::post('subtopic/{id}', 'HelpController@subtopicActions');
 
 
 // Add group.
-Route::post('ajax/groups/add/{id}', 'HelpController@addGroup');
+Route::post('group/add/{id}', 'HelpController@addGroup');
 
 // Remove group.
-Route::post('ajax/groups/remove/{id}', 'HelpController@removeGroup');
+Route::post('group/remove/{id}', 'HelpController@removeGroup');
