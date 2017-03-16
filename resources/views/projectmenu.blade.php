b5a3de6575763c0e41e9e3bd347d7567462e2056
bas2 <me@bashir.biz>
2017-03-16 23:39:25 +0000

    Change to correct link for project menu items.

--- resources/views/projectmenu.blade.php
+++ resources/views/projectmenu.blade.php
@@ -3,7 +3,7 @@
   @if (\Config::get('constants.appname')==$project)
   <li class="sel">{{ $project }}</li>
   @else
-  <li><a href="/{{ strtolower($project) }}/public/home">{{ $project }}</a></li>
+  <li>{{ link_to('../../../'.strtolower($project).'/public/home',$project) }}</li>
   @endif
 @endforeach
 </ul>
