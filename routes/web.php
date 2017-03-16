3af6b2be58b12a62a62bd12287fce9c2f499f463
bas2 <me@bashir.biz>
2017-03-16 20:05:50 +0000

    Layout changes.

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,18 @@
 <?php
-
-//use App\Http\Requests;
-
 Route::get('home', 'HelpController@index');
 
-// Complete article update:
-Route::post('article/edit/{id}', 'HelpController@postArticleEdit');
+Route::post('article/edit/{id}','HelpController@postArticleEdit'); #Complete article update
+Route::get('article/edit/{id}', 'HelpController@getArticleEdit'); #Display article edit form
+Route::get('article/subtopic/{id}', 'HelpController@articles' ); #Get subtopic articles
+Route::get('article/{id}', 'HelpController@article'); #Get article contents
+Route::get('article/reply/{id}', 'HelpController@replyToArticle'); #Add new reply
+Route::get('article/delete/{id}', 'HelpController@deleteArticle'); #Delete article
+Route::get('article/new/subtopic/{id}', 'HelpController@addArticle'); #New article
 
-// Display article edit form.
-Route::get('article/edit/{id}', 'HelpController@getArticleEdit');
+Route::get( 'subtopic/new/{id}', 'HelpController@getAddSubtopic'); #New subtopic
+Route::post('subtopic/new/{id}', 'HelpController@addSubtopic');   #New subtopic
+Route::get('subtopic/{id}', 'HelpController@getSubtopicActions'); #Rename or delete subtopic page
+Route::post('subtopic/{id}', 'HelpController@subtopicActions'); #Rename or delete subtopic complete
 
-// Get subtopic articles:
-Route::get('article/subtopic/{id}', 'HelpController@articles' );
-
-// Get article contents.
-Route::get('article/{id}', 'HelpController@article');
-
-// Add new reply:
-Route::get('article/reply/{id}', 'HelpController@replyToArticle');
-
-// Delete article
-Route::get('article/delete/{id}', 'HelpController@deleteArticle');
-
-// New article
-Route::get('article/new/subtopic/{id}', 'HelpController@addArticle' );
-
-
-// New subtopic.
-Route::get('subtopic/new/{id}', 'HelpController@getAddSubtopic');
-
-// New subtopic:
-Route::post('subtopic/new/{id}', 'HelpController@addSubtopic');
-
-// Rename or delete subtopic page:
-Route::get('subtopic/{id}', 'HelpController@getSubtopicActions');
-
-// Rename or delete subtopic complete:
-Route::post('subtopic/{id}', 'HelpController@subtopicActions');
-
-
-// Add group.
-Route::post('group/add/{id}', 'HelpController@addGroup');
-
-// Remove group.
-Route::post('group/remove/{id}', 'HelpController@removeGroup');
+Route::post('group/add/{id}', 'HelpController@addGroup');       #Add group.
+Route::post('group/remove/{id}', 'HelpController@removeGroup'); #Remove group.
