5de27d267525cde93f31bfd1623279f92451bb3f
bas2 <me@bashir.biz>
2017-05-12 17:10:28 +0100

    Added viewport meta

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ config('app.locale') }}">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>PHP App::Help!</title>
     <!-- Bootstrap -->
   {!! Html::style('css/bootstrap.min.css') !!}
