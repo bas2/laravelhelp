76973661c9e8027c671f9874605bbfe4476c8116
bas2 <me@bashir.biz>
2018-03-21 19:19:36 +0000

    text spacing

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ Route::get('/', 'HelpController@index')->name('HomeScreen');
 Route::post('article/edit/{id}','HelpController@postArticleEdit'); #Complete article update
 Route::get('article/edit/{id}', 'HelpController@getArticleEdit'); #Display article edit form
 Route::get('article/subtopic/{id}', 'HelpController@articles' ); #Get subtopic articles
-Route::get('article/{id}', 'HelpController@article'); #Get article contents
-Route::get('article/reply/{id}', 'HelpController@replyToArticle'); #Add new reply
-Route::get('article/delete/{id}', 'HelpController@deleteArticle'); #Delete article
+Route::get('article/{id}',          'HelpController@article'); #Get article contents
+Route::get('article/reply/{id}',    'HelpController@replyToArticle'); #Add new reply
+Route::get('article/delete/{id}',   'HelpController@deleteArticle'); #Delete article
 Route::get('article/new/subtopic/{id}', 'HelpController@addArticle'); #New article
 
 Route::get( 'subtopic/new/{id}', 'HelpController@getAddSubtopic'); #New subtopic
